33a76e6a03b1abf23d64a84d09e78ca1fee6db01
apknation <atanasikafuka@gmail.com>
2026-07-16 18:39:28 +0300

    navigation

--- app/src/main/java/com/example/myapplication/ui/theme/Color.kt
+++ app/src/main/java/com/example/myapplication/ui/theme/Color.kt
@@ -7,16 +7,20 @@ val OceanPrimary = Color(0xFF0077B6)
 val OceanSecondary = Color(0xFF00B4D8)
 val OceanTertiary = Color(0xFF48CAE4)
 val OceanSurfaceLight = Color(0xFFF3F8FB) // Light blue tint
+val OceanSurfaceVariantLight = Color(0xFFE0EBF5)
 val OceanBackgroundLight = Color(0xFFFFFFFF)
 val OceanOnSurfaceLight = Color(0xFF023E8A)
+val OceanOnSurfaceVariantLight = Color(0xFF1B3B5A)
 
 // Dark Mode
 val OceanDarkPrimary = Color(0xFF48CAE4)
 val OceanDarkSecondary = Color(0xFF90E0EF)
 val OceanDarkTertiary = Color(0xFF00B4D8)
 val OceanDarkSurface = Color(0xFF0D1B2A) // Deep sea blue
+val OceanDarkSurfaceVariant = Color(0xFF1B2A3B)
 val OceanDarkBackground = Color(0xFF0B1420)
 val OceanDarkOnSurface = Color(0xFFE0FBFC)
+val OceanDarkOnSurfaceVariant = Color(0xFFA0AEBF)
 
 // Semantic/Supporting
 val WaterAqua = Color(0xFF2EC4B6)
